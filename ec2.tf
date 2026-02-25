8919e65b11357aeb652ce54cd81fcc3f9bd76f4b
Renu88 <44968236+Renu88@users.noreply.github.com>
2026-02-26 00:07:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-0d16415f0e2dc4b80"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_1.id
-  security_groups = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<EOF
 #!/bin/bash
@@ -13,8 +13,8 @@ EOF
 }
 
 resource "aws_instance" "app" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-0d16415f0e2dc4b80"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private_1.id
-  security_groups = [aws_security_group.app_sg.id]
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 }
