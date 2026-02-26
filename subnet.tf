64b5e793ee73bc766915c1f67fc986d0d0d20212
Renu88 <44968236+Renu88@users.noreply.github.com>
2026-02-26 16:52:55 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,14 +2,14 @@
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.three_tier_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.three_tier_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
 
@@ -17,11 +17,11 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.three_tier_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.three_tier_vpc.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
 }
